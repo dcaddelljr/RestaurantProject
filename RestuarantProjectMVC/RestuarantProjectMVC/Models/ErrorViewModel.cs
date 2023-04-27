c2b995dc3e643e7f3c440662cf70f523ce762a1c
dcaddelljr <125912758+dcaddelljr@users.noreply.github.com>
2023-04-27 09:31:36 -0500

    DeletedJsonMVC

---
+++ RestuarantProjectMVC/RestuarantProjectMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RestuarantProjectMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
